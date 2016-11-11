7abf348c13823b5d55d0be272c1f9c0637bb4249
Esteban Familiar <efamiliar@gmail.com>
2016-11-11 23:40:50 +0100

    Append copyright

--- Blynk_witty/Blynk_witty.ino
+++ Blynk_witty/Blynk_witty.ino
@@ -1,9 +1,25 @@
+/*
+   Blynk IoT testing for ESP8266E Witty card
+   Copyright (C) 2016 Esteban Familiar Rey
 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #include <BlynkSimpleEsp8266.h>
 
-
-char auth[] = "cf9193981ce4477d9833524b1f649e37";
-char WiFiNetwork[] = "wifi_sid";
+char auth[] = "your_auth_code";
+char WiFiNetwork[] = "wifi_ssid";
 char WiFiPassword[] = "@wifipassword";
 
 
